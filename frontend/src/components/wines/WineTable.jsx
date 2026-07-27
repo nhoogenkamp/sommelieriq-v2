d8f3c662c98ab28b4335cce4cf7d8f3ced8e35cc
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-07-27 18:15:30 +0100

    update winetable in Component for future scaling

---
+++ frontend/src/components/wines/WineTable.jsx
@@ -0,0 +1,39 @@
+function WineTable({ wines }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Grape</th>
+          <th>Country</th>
+          <th>Region</th>
+          <th>Year</th>
+          <th>Bottle</th>
+          <th>Price</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {/* Mapping each wine object to a table row.
+        https://react.dev/learn/rendering-lists */}
+        {wines.map((wine) => (
+          <tr key={wine.wine_id}>
+            <td>{wine.name}</td>
+            <td>{wine.wine_type}</td>
+            <td>{wine.grape}</td>
+            <td>{wine.country}</td>
+            <td>{wine.region}</td>
+            <td>{wine.year}</td>
+            <td>{wine.bottle_type}</td>
+            <td>€{Number(wine.price).toFixed(2)}</td>
+            <td>{wine.description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default WineTable;
--- frontend/src/pages/WineList.jsx
+++ frontend/src/pages/WineList.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getRestaurantWines } from "../api/wineApi";
+import WineTable from "../components/wines/WineTable";
+// Importing with curly braces and without if export default: https://react.dev/learn/importing-and-exporting-components
 
 // function to show react-dom information params: https://www.w3schools.com/React/showreact.asp?filename=demo_react_router_params
 function WineList() {
@@ -60,43 +62,10 @@ function WineList() {
       {wines.length === 0 ? (
         <p>No wines are currently available.</p>
       ) : (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Type</th>
-              <th>Grape</th>
-              <th>Country</th>
-              <th>Region</th>
-              <th>Year</th>
-              <th>Bottle</th>
-              <th>Price</th>
-              <th>Description</th>
-            </tr>
-          </thead>
-
-  {/* Mapping each wine object to a table row.
-      https://react.dev/learn/rendering-lists */}
-
-          <tbody>
-            {wines.map((wine) => (
-              <tr key={wine.wine_id}>
-                <td>{wine.name}</td>
-                <td>{wine.wine_type}</td>
-                <td>{wine.grape}</td>
-                <td>{wine.country}</td>
-                <td>{wine.region}</td>
-                <td>{wine.year}</td>
-                <td>{wine.bottle_type}</td>
-                <td>€{Number(wine.price).toFixed(2)}</td>
-                <td>{wine.description}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </main>
-  );
+      <WineTable wines={wines} />
+    )}
+  </main>
+);
 }
 
 export default WineList;
