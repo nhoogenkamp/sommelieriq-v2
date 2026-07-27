8463ed07b2138d37ef99d201248b5d297ca83ea7
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-07-27 19:02:24 +0100

    Filter functionality

--- frontend/src/pages/WineList.jsx
+++ frontend/src/pages/WineList.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getRestaurantWines } from "../api/wineApi";
 import WineTable from "../components/wines/WineTable";
+import WineFilters from "../components/wines/WineFilters";
 // Importing with curly braces and without if export default: https://react.dev/learn/importing-and-exporting-components
 
 // function to show react-dom information params: https://www.w3schools.com/React/showreact.asp?filename=demo_react_router_params
@@ -12,6 +13,11 @@ function WineList() {
   const [wines, setWines] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
+
+  // Filters
+  const [selectedColour, setSelectedColour] = useState("");
+  const [selectedBottle, setSelectedBottle] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
 
   //fetches the wines: https://react.dev/learn/synchronizing-with-effects#:~:text=its%20initial%20state.-,Fetching%20data,-If%20your%20Effect
 
@@ -33,6 +39,32 @@ function WineList() {
 
     loadWines();
   }, [restaurantId]);
+
+  const filteredWines = wines.filter((wine) => {
+    return (
+      // when selectedColour is empty, all colours are allowed
+      // otherwise, the wine type must match the selected colour
+      (selectedColour === "" ||
+        wine.wine_type === selectedColour) &&
+
+      // when selectedBottle is empty, all bottle types are allowed
+      // otherwise, the bottle type must match
+      (selectedBottle === "" ||
+        wine.bottle_type === selectedBottle) &&
+
+      // when maxPrice is empty, there is no price limit
+      // otherwise, the wine price must be below or equal to it
+      (maxPrice === "" ||
+        Number(wine.price) <= Number(maxPrice))
+    );
+  });
+
+  // changes all filters back to their original empty values
+  function clearFilters() {
+    setSelectedColour("");
+    setSelectedBottle("");
+    setMaxPrice("");
+  }
 
   if (isLoading) {
     return (
@@ -56,13 +88,22 @@ function WineList() {
   return (
     <main>
       <h1>Wine List</h1>
+      <WineFilters
+        selectedColour={selectedColour}
+        selectedBottle={selectedBottle}
+        maxPrice={maxPrice}
+        setSelectedColour={setSelectedColour}
+        setSelectedBottle={setSelectedBottle}
+        setMaxPrice={setMaxPrice}
+        clearFilters={clearFilters}
+      />
       {/* Conditionally render either a message or the wine table with the use of Conditional operator (? :) 
         https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_operator */}
       
-      {wines.length === 0 ? (
-        <p>No wines are currently available.</p>
+      {filteredWines.length === 0 ? (
+        <p>No wines are currently available or no wines match the selected filters..</p>
       ) : (
-      <WineTable wines={wines} />
+      <WineTable wines={filteredWines} />
     )}
   </main>
 );
