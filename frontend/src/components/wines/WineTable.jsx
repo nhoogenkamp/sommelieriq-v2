54ca6fa68b3dd29878b5351a5db3e7d987548c8a
nhoogenkamp <152394888+nhoogenkamp@users.noreply.github.com>
2026-08-02 10:22:32 +0100

    wine table adding classes to adjust CSS and have a view winePopup

--- frontend/src/components/wines/WineTable.jsx
+++ frontend/src/components/wines/WineTable.jsx
@@ -1,17 +1,19 @@
+import WinePopup from "./WinePopup";
+
 function WineTable({ wines }) {
   return (
     <table className="wine-table">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Type</th>
-          <th>Grape</th>
-          <th>Country</th>
-          <th>Region</th>
-          <th>Year</th>
-          <th>Bottle</th>
-          <th>Price</th>
-          <th>Description</th>
+          <th className="wine-name">Name</th>
+          <th className="wine-type">Type</th>
+          <th className="wine-grape">Grape</th>
+          <th className="wine-country">Country</th>
+          <th className="wine-region">Region</th>
+          <th className="wine-year">Year</th>
+          <th className="wine-bottle">Bottle</th>
+          <th className="wine-price">Price</th>
+          <th className="wine-view">View</th>
         </tr>
       </thead>
 
@@ -20,15 +22,17 @@ function WineTable({ wines }) {
         https://react.dev/learn/rendering-lists */}
         {wines.map((wine) => (
           <tr key={wine.wine_id}>
-            <td>{wine.name}</td>
-            <td>{wine.wine_type}</td>
-            <td>{wine.grape}</td>
-            <td>{wine.country}</td>
-            <td>{wine.region}</td>
-            <td>{wine.year}</td>
-            <td>{wine.bottle_type}</td>
-            <td>€{Number(wine.price).toFixed(2)}</td>
-            <td>{wine.description}</td>
+            <td className="wine-name">{wine.name}</td>
+            <td className="wine-type">{wine.wine_type}</td>
+            <td className="wine-grape">{wine.grape}</td>
+            <td className="wine-country">{wine.country}</td>
+            <td className="wine-region">{wine.region}</td>
+            <td className="wine-year">{wine.year}</td>
+            <td className="wine-bottle">{wine.bottle_type}</td>
+            <td className="wine-price">€{Number(wine.price).toFixed(2)}</td>
+
+            {/* Opens the popup for this wine */}
+            <td className="wine-view"><WinePopup wine={wine} /></td>
           </tr>
         ))}
       </tbody>
